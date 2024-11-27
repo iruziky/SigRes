5f23553e43388f3224c0dc684b92346154a6b8e8
iruziky <iruziky@gmail.com>
2024-11-27 12:18:43 -0300

    adicionando novos pratos de exemplo

--- src/components/Blocos/Produtos/Produtos.js
+++ src/components/Blocos/Produtos/Produtos.js
@@ -5,12 +5,41 @@ import Produto from '../../Elementos/Produto/Produto'
 export default function Produtos() {
     return (
         <div className='Produtos'>
-            <Produto nomeProduto={"Salmão com Molho de Maracujá"} preco={"49,90"} iconePath={"/images/garfo.svg"}/>
-            <Produto nomeProduto={"Espaguete à Carbonara"} preco={"39,90"} iconePath={"/images/garfo.svg"}/>
-            <Produto nomeProduto={"Hambúrguer Artesanal"} preco={"24,90"} iconePath={"/images/garfo.svg"}/>
-            <Produto nomeProduto={"Pizza Marguerita"} preco={"39,90"} iconePath={"/images/garfo.svg"}/>
-            <Produto nomeProduto={"Risoto de Cogumelos"} preco={"42,00"} iconePath={"/images/garfo.svg"}/>
-            <Produto nomeProduto={"Lasanha de Carne"} preco={"34,90"} iconePath={"/images/garfo.svg"}/>
+            <Produto nomeProduto={"Salmão com Molho de Maracujá"} preco={"49,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Espaguete à Carbonara"} preco={"39,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Hambúrguer Artesanal"} preco={"24,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Pizza Marguerita"} preco={"39,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Risoto de Cogumelos"} preco={"42,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Lasanha de Carne"} preco={"34,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Frango Grelhado com Batatas"} preco={"29,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Tacos de Carne de Sol"} preco={"27,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Bife à Parmegiana"} preco={"38,50"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Peixe Assado com Ervas"} preco={"52,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Fettuccine ao Molho Alfredo"} preco={"36,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Sopa de Abóbora com Gengibre"} preco={"18,50"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Camarão ao Alho e Óleo"} preco={"48,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Risoto de Frango com Limão"} preco={"45,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Pão de Alho com Queijo"} preco={"15,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Costela de Porco Assada"} preco={"55,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Pizza de Calabresa"} preco={"40,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Sanduíche Natural de Frango"} preco={"22,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Crepe de Presunto e Queijo"} preco={"26,50"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Salada de Quinoa com Abacate"} preco={"32,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Escondidinho de Carne Seca"} preco={"43,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Picanha Grelhada com Farofa"} preco={"59,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Massa com Molho Pesto"} preco={"34,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Bacalhau à Brás"} preco={"65,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Burguer Vegano"} preco={"28,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Pizza de Frango com Catupiry"} preco={"43,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Churrasco de Linguicinha"} preco={"37,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Moussaka Grega"} preco={"50,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Torta de Maçã com Canela"} preco={"18,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Pavê de Chocolate e Morango"} preco={"22,50"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Pudim de Leite Condensado"} preco={"16,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Brownie de Chocolate com Nozes"} preco={"20,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Bolo de Cenoura com Cobertura de Chocolate"} preco={"25,00"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Mousse de Limão"} preco={"18,90"} iconePath={"/images/garfo.svg"} />
+            <Produto nomeProduto={"Tiramisu"} preco={"28,00"} iconePath={"/images/garfo.svg"} />
         </div>
     )
 }
